a54401e7485f697ed45dc4c2b21975f9bba5467e
PiThetaProductions <psargent@Philips-MacBook-Pro.local>
2022-04-23 14:24:39 -0700

    ms

--- EmailFileRead.cs
+++ EmailFileRead.cs
@@ -206,7 +206,7 @@ namespace EmailReader //rename
                 fileName = fileName1;
 
             String myString = File.ReadAllText(fileName);
-            string toBeSearched = DateTime.Now.AddDays(-1 * day).ToString("MM/dd/yyyy") + ":\n";
+            string toBeSearched = DateTime.Now.AddDays(-1 * day).ToString("MM/dd/yyyy") + ":";
             int ix = myString.IndexOf(toBeSearched);
 
             if (ix != -1)
@@ -216,7 +216,7 @@ namespace EmailReader //rename
             }
             else
             {
-                toBeSearched = DateTime.Now.ToString("MM/dd/yyyy") + ":\n";
+                toBeSearched = DateTime.Now.ToString("MM/dd/yyyy") + ":";
                 ix = myString.IndexOf(toBeSearched);
 
                 if (ix != -1)
@@ -238,7 +238,7 @@ namespace EmailReader //rename
 
             String myString = File.ReadAllText(fileName);
 
-            string toBeSearched = myDate.ToString("MM/dd/yyyy") + ":\n";
+            string toBeSearched = myDate.ToString("MM/dd/yyyy") + ":";
             string toBeSearched2 = myDate.AddDays(1).ToString("MM/");
 
             int ix = myString.IndexOf(toBeSearched);
@@ -262,7 +262,7 @@ namespace EmailReader //rename
             }
             else
             {
-                toBeSearched = DateTime.Now.ToString("MM/dd/yyyy") + ":\n";
+                toBeSearched = DateTime.Now.ToString("MM/dd/yyyy") + ":";
                 ix = myString.IndexOf(toBeSearched);
                 if (ix != -1)
                 {
